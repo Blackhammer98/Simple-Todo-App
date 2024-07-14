a8e73e4d822c851cbfc534faa2543c0f9a20cf56
nikit <snikit086@gmail.com>
2024-07-14 16:24:10 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const { string, boolean } = require("zod")
 
-mongoose.connect("mongodb+srv://officialnikit120598:IitrhEw5JR0dYLNg@cluster0.y0tr6dt.mongodb.net/Todo_Db")
+mongoose.connect("")
 const todoSchema  = mongoose.Schema({
   title : String,
   description : String,
